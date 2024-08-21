0b6700332c4b6aa3a89f0f86cd15ef7f1ffd9fa2
052787 <052787@SPFAM2LAB164-00.famacad.com.br>
2024-08-20 21:35:13 -0300

    Aula 20/8

--- script.sql
+++ script.sql
@@ -15,3 +15,21 @@ insert into Funcionarios values( 3, 'Ana', '2024-02-10', 5000, 'ana@teste.com');
 insert into Funcionarios values( 4, 'Cláudia', '2024-01-15', 6000, 'claudia@teste.com');
 
 insert into Funcionarios values( 5, 'Pedro', '2024-07-25', 4500, 'pedro@teste.com');
+
+create table Alunos(
+  ra int,
+  nome varchar(50),
+  dataNascimento date,
+  renda decimal(15,2),
+  email varchar(200),
+  primary key(ra));
+
+insert into Alunos
+values(102,'Joana','2000-05-01',5000,'joana@teste');
+
+update alunos
+set renda = 5000, email='joao@email'
+where ra = 100;
+
+delete from alunos where ra = 102
+
